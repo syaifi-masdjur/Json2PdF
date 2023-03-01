21fa44e5734b7a5949101caee587d19eb3642f5d
SyaifiMasdjur <syaifi@gmail.com>
2023-03-01 19:21:11 +0700

    Initialize Fonts Toolbar

--- Json2PdF.Window/App.xaml.cs
+++ Json2PdF.Window/App.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Json2PdF.Window.ViewModels;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
@@ -13,5 +18,44 @@ namespace Json2PdF.Window
     /// </summary>
     public partial class App : Application
     {
+
+
+        public static IHost? Host { get; private set; }
+
+        public App()
+        {
+            Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    ConfigureServices(services);
+                })
+                .Build();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Add Services
+            services.AddSingleton<MainViewModel, MainViewModel>();
+            services.AddSingleton<MainWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await Host!.StartAsync();
+
+            var mainViewModel = Host.Services.GetRequiredService<MainViewModel>();
+            var mainWindow = Host.Services.GetRequiredService<MainWindow>();
+            mainWindow.DataContext = mainViewModel;
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await Host!.StopAsync();
+            base.OnExit(e);
+        }
+      
     }
 }
---
+++ Json2PdF.Window/ViewModels/MainViewModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+namespace Json2PdF.Window.ViewModels
+{
+    public class MainViewModel
+    {
+
+        public MainViewModel()
+        {
+            _fonts = new List<FontFamily>();
+            _fontStyle = new Dictionary<int, string>();
+            _fontSize = new List<int>();
+
+            initialFonts();
+            _selectedFont = _fonts.FirstOrDefault();
+
+            initialStyle();
+            _selectedFontStyle = _fontStyle.First();
+
+            initializeFontSize();
+        }
+
+
+        private void initialFonts()
+        {
+
+            InstalledFontCollection  installedFont = new InstalledFontCollection();
+
+            foreach (var font in installedFont.Families)
+            {
+                _fonts.Add(font);
+            }
+        }
+
+
+        private IList<FontFamily> _fonts;
+
+        public IList<FontFamily> Fonts
+        {
+            get { return _fonts; }
+            set { _fonts = value; }
+        }
+
+        private FontFamily? _selectedFont;
+
+        public FontFamily? SelectedFont
+        {
+            get { return _selectedFont; }
+            set { _selectedFont = value; }
+        }
+
+
+        private void initialStyle()
+        {
+            _fontStyle.Add(Convert.ToInt16(FontStyle.Regular), FontStyle.Regular.ToString());
+            _fontStyle.Add(Convert.ToInt16(FontStyle.Bold), FontStyle.Bold.ToString());
+            _fontStyle.Add(Convert.ToInt16(FontStyle.Italic), FontStyle.Italic.ToString());
+            _fontStyle.Add(Convert.ToInt16(FontStyle.Underline), FontStyle.Underline.ToString());
+            _fontStyle.Add(Convert.ToInt16(FontStyle.Strikeout), FontStyle.Strikeout.ToString());
+        }
+
+        private Dictionary<int, string> _fontStyle;
+        public Dictionary<int, string> FontStyles
+        {
+            get { return _fontStyle; }
+            set { _fontStyle = value; }
+        }
+
+        private KeyValuePair<int,string> _selectedFontStyle;
+
+        public KeyValuePair<int, string> SelectedFontStyle
+        {
+            get { return _selectedFontStyle; }
+            set { _selectedFontStyle = value; }
+        }
+
+
+        private void initializeFontSize()
+        {
+            _fontSize.Add(8);
+            _fontSize.Add(9);
+            _fontSize.Add(10);
+            _fontSize.Add(11);
+            _fontSize.Add(12);
+            _fontSize.Add(14);
+            _fontSize.Add(16);
+            _fontSize.Add(18);
+            _fontSize.Add(20);
+            _fontSize.Add(22);
+            _fontSize.Add(24);
+            _fontSize.Add(26);
+            _fontSize.Add(28);
+            _fontSize.Add(36);
+            _fontSize.Add(72);
+        }
+
+        private IList<int> _fontSize;
+
+        public IList<int> FontSize
+        {
+            get { return _fontSize; }
+            set { _fontSize = value; }
+        }
+
+
+    }
+}
