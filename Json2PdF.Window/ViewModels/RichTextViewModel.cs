e1c868c1ac840c0c0d18769f874f1bde850bf3c0
SyaifiMasdjur <syaifi@gmail.com>
2023-03-06 16:57:32 +0700

    Setting and checking active font on text editor

--- Json2PdF.Window/ViewModels/RichTextViewModel.cs
+++ Json2PdF.Window/ViewModels/RichTextViewModel.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Accessibility;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Json2PdF.Window.ViewModels
 {
-    class RichTextViewModel
+    public class RichTextViewModel
     {
+        public RichTextViewModel()
+        {
+
+        }
+
+        private FontFamily _activeFont;
+
+        public FontFamily ActiveFont
+        {
+            get { return _activeFont; }
+            set { _activeFont = value; }
+        }
+
+        private int _activeFontStyle;
+
+        public int ActiveFontStyle
+        {
+            get { return _activeFontStyle; }
+            set { _activeFontStyle = value; }
+        }
+
+        private int _activeFontSize;
+
+        public int ActiveFontSize
+        {
+            get { return _activeFontSize; }
+            set { _activeFontSize = value; }
+        }
+
+
     }
 }
